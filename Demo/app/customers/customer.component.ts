6480e1abee78aba220ec5aa5cd7990e38e7e78e4
Rupam Roy <Rupam.Roy@genworth.com>
2017-07-30 17:08:52 -0400

    Done with module 2

--- Demo/app/customers/customer.component.ts
+++ Demo/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -7,12 +7,30 @@ import { Customer } from './customer';
     selector: 'my-signup',
     templateUrl: './app/customers/customer.component.html'
 })
-export class CustomerComponent  {
-    customer: Customer= new Customer();
-    customerForm: FormGroup
+export class CustomerComponent implements OnInit { 
+    customer: Customer = new Customer();
+    customerForm: FormGroup;
+
+    constructor(private fb: FormBuilder){}
+
+    ngOnInit(): void {
+        this.customerForm = this.fb.group({
+            firstName: '',
+            lastName: '',
+            email: '',
+            sendCatalog: true
+        });
+    }
+
+    populateTestData(): void {
+        this.customerForm.patchValue({
+            firstName: 'Rupam',
+            lastName: 'Roy'
+        });
+    }
 
     save() {
         console.log(this.customerForm);
-        console.log('Saved: ' + JSON.stringify(customerForm.value));
+        console.log('Saved: ' + JSON.stringify(this.customerForm.value));
     }
  }
