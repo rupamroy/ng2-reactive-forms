9158f37a85d6e92f2e41838bfd9f089dc710ce9f
Rupam Roy <Rupam.Roy@genworth.com>
2017-08-19 16:22:42 -0400

    Done with Dynamically duplicating input elements

--- Demo/app/customers/customer.component.ts
+++ Demo/app/customers/customer.component.ts
@@ -69,6 +69,10 @@ export class CustomerComponent implements OnInit {
         });
     }
 
+    addAddress(): void {
+        this.addresses.push(this.buildAddress());
+    }
+
     buildAddress(): FormGroup {
         return this.fb.group({
             addressType: 'home',
